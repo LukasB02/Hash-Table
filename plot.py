c6111a7e7ee143be429d552e471c1f85a24dbbb9
P4ale <justus.neubauer@gmx.at>
2026-03-15 20:03:30 +0100

    plot implemented

---
+++ plot.py
@@ -0,0 +1,28 @@
+import price
+
+def plot(prices):
+
+    low = min(prices)
+    high = max(prices)
+    diff = high - low
+    middle = (low + high) / 2
+    row = diff / 10
+
+    print("Highest Price: ", high, "Lowest Price: ", low)
+
+    for i in range(10):
+        level = high - (row * i)
+
+        if i == 5:
+            print(f"{middle:6.2f} |", end="")
+        elif i == 9:
+            print(f"{low:6.2f} |", end="")
+        else:
+            print("       |", end="")
+
+        for p in prices:
+            if level <= p < level + row:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
